d1de432c83988e8ca548b24aa904289aafdacfe8
Michal Novak <mnovak@ufal.mff.cuni.cz>
2015-07-15 01:31:08 +0200

    gazetteers builder ready for Czech

--- gazetteer_builder/extract_scripts/common.sh
+++ gazetteer_builder/extract_scripts/common.sh
@@ -11,8 +11,7 @@ function download_xz {
 function download_svn {
     url=$1
     tmp_dir=$2
-    url=svn://anonsvn.kde.org/home/kde/branches/stable/l10n-kde4/cs/messages
-    svn co $url
+    svn co $url $tmp_dir
 }
 
 function build_gazeteers {
@@ -20,7 +19,12 @@ function build_gazeteers {
     en_gazfile=$2
     otherlang_gazfile=$3
     id_prefix=$4
-    find $po_path -name '*.po' -exec cat {} \; | \
+    tmp_dir=${5-.}
+    if [[ "$po_path" == *.po ]]; then
+        cat $tmp_dir/$po_path
+    else
+        find $tmp_dir/$po_path -name '*.po' -exec cat {} \;
+    fi | \
         `dirname $BASH_SOURCE`/po2gaz.pl $en_gazfile $otherlang_gazfile $id_prefix
 }
 
