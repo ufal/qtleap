a515d1c7a74a884f00544bcbb2b0be6626e9e406
Michal Novak <mnovak@ufal.mff.cuni.cz>
2015-08-21 11:01:02 +0200

    gazetteer extractor for Rosa's wiki lists; so far, for Czech only

--- gazetteer_builder/extract_scripts/common.sh
+++ gazetteer_builder/extract_scripts/common.sh
@@ -4,7 +4,7 @@ function download_xz {
     tmp_dir=${3-.}
     xz_file=${url##*/}
     cd $tmp_dir
-    wget $URL
+    wget $url
     tar -xvvJf $xz_file $subdir
     cd ~-
 }
@@ -13,6 +13,19 @@ function download_svn {
     url=$1
     tmp_dir=$2
     svn co $url $tmp_dir
+}
+
+function extract_zip {
+    zip_file=$1
+    subdir=$2
+    tmp_dir=${3-.}
+    cd $tmp_dir
+    if [ ! -e $zip_file ]; then
+        echo "The file $tmp_dir/$zip_file is missing. Ask Michal Novák <mnovak@ufal.mff.cuni.cz> to get it." >&2
+        exit 1
+    fi
+    unzip $zip_file $subdir
+    cd ~-
 }
 
 function build_gazeteers {
@@ -27,6 +40,15 @@ function build_gazeteers {
         find $tmp_dir/$po_path -name '*.po' -exec cat {} \;
     fi | \
         `dirname $BASH_SOURCE`/po2gaz.pl $en_gazfile $otherlang_gazfile $id_prefix
+}
+
+function build_gazetteers_from_rosas_tsv {
+    tsv_path=$1
+    en_gazfile=$2
+    otherlang_gazfile=$3
+    id_prefix=$4
+    tmp_dir=${5-.}
+    `dirname $BASH_SOURCE`/rosa_tsv2gaz.pl $en_gazfile $otherlang_gazfile $id_prefix < $tmp_dir/$tsv_path
 }
 
 #if [ "_$4" == "clean" ]; then
