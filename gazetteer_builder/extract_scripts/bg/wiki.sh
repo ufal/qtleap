d64caf0b445a37d498e8ff06f2b30f303eb14e94
Michal Novak <mnovak@ufal.mff.cuni.cz>
2015-09-01 08:09:08 +0200

    extractors for non-TMT languages added

---
+++ gazetteer_builder/extract_scripts/bg/wiki.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source `dirname $BASH_SOURCE`/../common.sh
+
+DOWNLOADED_FILE=gazetteers.zip
+TSV_PATH=gazetteerBG.tsv
+
+EN_GAZFILE=$1
+OTHERLANG_GAZFILE=$2
+ID_PREFIX=$3
+
+TMP_DIR=$4
+
+extract_zip $DOWNLOADED_FILE $TSV_PATH $TMP_DIR
+build_gazetteers_from_rosas_tsv $TSV_PATH $EN_GAZFILE $OTHERLANG_GAZFILE $ID_PREFIX $TMP_DIR
