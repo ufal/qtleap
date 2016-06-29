e867c2a076b73b3532baecd59612f3f9d7afd88a
Gorka Labaka Intxauspe <gorka.labaka@ehu.eus>
2016-06-29 15:38:38 +0200

    treelet dictionary to incorporate Microsoft Terminology dictionary

---
+++ treelet-dict/bin/tbx2tsv.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use Treex::Block::T2T::TbxParser;
+
+$num_args = $#ARGV + 1;
+if ($num_args != 3) {
+    print "\nUsage: tbx2tsv.pl dictionary src-id trg-id\n";
+    exit;
+}
+
+my @entries = Treex::Block::T2T::TbxParser->parse_tbx($ARGV[0], $ARGV[1], $ARGV[2]);
+foreach my $entry (@entries) {
+    print $entry->{SRC_TEXT} . "\t" . $entry->{TRG_TEXT} . "\n";
+}
